04ff0a60e48282fe2484520777837d6ec14175b9
boom210232 <chonchanok.che@ku.th>
2021-11-29 23:33:48 +0700

    add get_link function

--- link_scan.py
+++ link_scan.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from urllib.request import urlopen
 from urllib import error
 from selenium.webdriver.common.by import By
-from urllib.parse import urlparse
 import sys
 
 
@@ -13,8 +12,26 @@ def get_links(url):
         a list of all unique hyperlinks on the page,
         without page fragments or query parameters.
     """
-    pass
-
+    result = []
+    compare = []
+    browser = webdriver.Chrome('/Users/boom/Desktop/link-scanner/chromedriver')
+    # Change to your own path in bracket.
+    browser.get(url)
+    context = browser.find_elements(By.CSS_SELECTOR, 'a[href^="http"]')
+    for i in context:
+        # print(i)
+        v_url = str(i.get_attribute('href'))
+        compare.append(v_url)
+        if v_url.__contains__('?') or v_url.__contains__('#'):
+            question_split = v_url.split('?')
+            hashtag_split = question_split[0].split('#')
+            if hashtag_split[0] not in result:
+                result.append(hashtag_split[0])
+        else:
+            if v_url not in result:
+                result.append(v_url)
+    # print(compare)
+    return result
 
 
 def is_valid_url(url: str):
@@ -30,7 +47,7 @@ def is_valid_url(url: str):
     return True
 
 
-def invalid_urls(urllist):
+def invalid_urls(urllist: list) -> list:
     """Validate the urls in urllist and return a new list containing
     the invalid or unreachable urls.
     """
@@ -38,13 +55,14 @@ def invalid_urls(urllist):
     for all_url in urllist:
         if not is_valid_url(all_url):
             invalid_links.append(all_url)
-    print(invalid_links)
+    # print(invalid_links)
     return invalid_links
 
 
 if __name__ == "__main__":
     # print(is_valid_url("https://www.thaizeed.net/"))
     # print(is_valid_url("https://www.maimeeyoujing.co.th/"))
-    # invalid_urls(["https://www.thaizeed.net/","https://www.maimeeyoujing.co.th/"])
-
-
+    # print(invalid_urls(["https://www.thaizeed.net/","https://www.maimeeyoujing.co.th/"]))
+    # print(get_links('https://cpske.github.io/ISP'))
+    # print(get_links('http://anicheck-isp.herokuapp.com/'))
+    pass
